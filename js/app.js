038dc2e9f1d335fa3fa71ce418fab1d9c3315ea4
Kenya Womack <ladykamillion27@gmail.com>
2023-03-09 20:09:01 -0600

    prob3

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ Test this function by hand in the console to get it working, and when you think 
 //  this is problem1
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    let mySum = a+b;
+    let mySum = a + b;
     let sentence = `The sum of ${a} and ${b} is ${mySum}.`;
     return [mySum, sentence];
-//     return [a+b,  `The sum of ${a} and ${b} is ${a+b}]`
+    //     return [a+b,  `The sum of ${a} and ${b} is ${a+b}]`
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -30,14 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    let myProduct = a*b;
+    let myProduct = a * b;
     let answer = `The product of ${a} and ${b} is ${myProduct}.`;
-    return [myProduct, answer]; 
+    return [myProduct, answer];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
-   
+testMultiply(5, 9);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,11 +54,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    let sumAB = sum (a, b)[0];
+    let sumTotal = sum (sumAB, c)[0];
+    let productAB = multiply (a, b)[0];
+    let productTotal = multiply (productAB, c)[0];
+    let message1 = `${a} and ${b} and ${c} sum to ${sumTotal}.`;
+    let message2 = `The product of ${a} and ${b} and ${c} is ${productTotal}.`;
+    return[sumTotal, productTotal, message1, message2]; 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -72,6 +78,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
+// test
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
@@ -122,7 +129,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
